7985a01a81e7bfc10f2a163a9b2bc240c41185c9
Eric Do <ericdo.617@gmail.com>
2023-01-05 17:55:10 -0600

    Configure absolute pathing and initial setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  devtool: 'source-map',
+  resolve: {
+    modules: [path.resolve(__dirname, './client/src'), 'node_modules'],
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, './client/src'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(css)$/,
+        use: ['css-loader']
+      }
+    ]
+  }
+};
